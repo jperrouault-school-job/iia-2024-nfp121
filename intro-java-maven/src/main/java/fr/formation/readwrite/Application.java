6ec07c43a15cf69594df1a8201f806fdc65e9a26
Jeremy <jeremy.perrouault@ascadis.fr>
2024-03-19 12:27:30 +0100

    JOUR 2 - TP WRITE FILE

--- intro-java-maven/src/main/java/fr/formation/readwrite/Application.java
+++ intro-java-maven/src/main/java/fr/formation/readwrite/Application.java
@@ -3,10 +3,16 @@ package fr.formation.readwrite;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Application {
     public static void main(String[] args) {
+        read();
+        write();
+    }
+
+    public static void read() {
         File file = new File("hello.txt");
 
         // Le "try-with-resource" va automatiquement appeler la méthode "close" à la fin du try
@@ -23,6 +29,18 @@ public class Application {
         
         catch (IOException e) {
             System.out.println("Impossible de lire le contenu ...");
+        }
+    }
+
+    public static void write() {
+        File file = new File("bye.txt");
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+            fileOutputStream.write("Au revoir !".getBytes());
+        }
+
+        catch (IOException ex) {
+            System.out.println("Impossible d'écrire dans le fichier ...");
         }
     }
 }
