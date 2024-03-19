e94c3fbe90edbf2bb80ecc6989b13a28b50b7de0
Jeremy <jeremy.perrouault@ascadis.fr>
2024-03-19 09:31:03 +0100

    JOUR 2 - TP PROBLEM BUILDER

--- intro-java/src/fr/formation/Personne.java
+++ intro-java/src/fr/formation/Personne.java
@@ -1,6 +1,88 @@
 package fr.formation;
 
+import java.time.LocalDate;
+
 public class Personne {
+    private String nom;
+    private String prenom;
+    private LocalDate dateNaissance;
+    private String email;
+    private String username;
+    private String password;
+
+    public Personne(String nom, String prenom, LocalDate dateNaissance, String email, String username, String password) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Personne(String nom, String prenom, LocalDate dateNaissance) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+    }
+
+    public Personne(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    public Personne(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public LocalDate getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(LocalDate dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public void parler() {
         System.out.println("La personne parle ...");
     }
