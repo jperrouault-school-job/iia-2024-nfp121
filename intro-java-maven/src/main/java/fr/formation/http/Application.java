59eabfd33c0c1b8f9a6877a9ff52ec62f578f587
Jeremy <jeremy.perrouault@ascadis.fr>
2024-03-19 13:44:52 +0100

    JOUR 2 - DEMO HttpClient

---
+++ intro-java-maven/src/main/java/fr/formation/http/Application.java
@@ -0,0 +1,34 @@
+package fr.formation.http;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandler;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class Application {
+    public static void main(String[] args) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("https://www.google.fr"))
+                .GET()
+                .build()
+            ;
+
+            HttpClient httpClient = HttpClient.newBuilder().build();
+            HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
+
+            System.out.println("Le résultat est : " + response.body());
+        }
+
+        catch (URISyntaxException ex) {
+            System.out.println("L'URI spécifiée est incorrecte.");
+        }
+
+        catch (Exception ex) {
+            System.out.println("Problème lors de l'exécution de la requête ...");
+        }
+    }
+}
