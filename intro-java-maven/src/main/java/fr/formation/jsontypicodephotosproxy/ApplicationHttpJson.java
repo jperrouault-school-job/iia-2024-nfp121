de239231382991d2654af4bfcf178957e0967051
Jeremy <jeremy.perrouault@ascadis.fr>
2024-03-20 14:37:58 +0100

    JOUR 3 - TP HTTP CLIENT JSON

--- intro-java-maven/src/main/java/fr/formation/jsontypicodephotosproxy/ApplicationHttpJson.java
+++ intro-java-maven/src/main/java/fr/formation/jsontypicodephotosproxy/ApplicationHttpJson.java
@@ -1,18 +1,7 @@
 package fr.formation.jsontypicodephotosproxy;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.Arrays;
 import java.util.List;
-
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import fr.formation.ObjectMapperFactory;
 
 public class ApplicationHttpJson {
     public static void main(String[] args) {
