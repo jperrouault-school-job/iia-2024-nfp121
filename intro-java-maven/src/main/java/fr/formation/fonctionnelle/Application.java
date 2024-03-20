1699fb7bfda56b8529c180a82c275d6eba3c6914
Jeremy <jeremy.perrouault@ascadis.fr>
2024-03-20 16:26:48 +0100

    JOUR 3 - TP FONCTIONNELLE

--- intro-java-maven/src/main/java/fr/formation/fonctionnelle/Application.java
+++ intro-java-maven/src/main/java/fr/formation/fonctionnelle/Application.java
@@ -1,9 +1,51 @@
 package fr.formation.fonctionnelle;
 
+import java.util.Arrays;
 import java.util.List;
+
+import fr.formation.jsontypicodephotosproxy.HttpJsonClient;
+import fr.formation.jsontypicodephotosproxy.Photo;
 
 public class Application {
     public static void main(String[] args) {
+        List<String> prenoms = List.of("Enzo", "Yann", "Jérémy", "Aurélien");
+
+        // prenoms.forEach((prenom) -> {
+        //     System.out.println(prenom);
+        // });
+        
+        // prenoms.forEach(prenom -> System.out.println(prenom));
+
+        // prenoms.forEach(System.out::println);
+        prenoms.stream()
+            .filter(prenom -> !prenom.equals("Aurélien"))
+            // .map(prenom -> prenom.toUpperCase())
+            .map(String::toUpperCase)
+            // .forEach(prenom -> System.out.println(prenom))
+            .forEach(System.out::println);
+        ;
+
+
+        // Liste des photos
+        // On garde les IDS paires < 150
+        // On récupère que les titres
+        // On les affiche
+
+        List<Photo> photos = Arrays.asList(
+            new HttpJsonClient().get("https://jsonplaceholder.typicode.com/photos", Photo[].class)
+        );
+        
+        List<String> titles = photos.stream()
+            .filter(p -> (p.getId() < 150) && (p.getId() % 2 == 0))
+            // .map(p -> p.getTitle())
+            .map(Photo::getTitle)
+            .toList()
+        ;
+
+        titles.forEach(System.out::println);
+    }
+
+    public static void demo() {
         IfaceFonctionnelle demo1 = new IFaceFonctionnelleImpl();
 
         demo1.demo();
@@ -25,17 +67,6 @@ public class Application {
 
         demo3.demo();
         demo3.fncDemo();
-
-
-        List<String> prenoms = List.of("Enzo", "Yann", "Jérémy", "Aurélien");
-
-        prenoms.forEach((prenom) -> {
-            System.out.println(prenom);
-        });
-        
-        prenoms.forEach(prenom -> System.out.println(prenom));
-
-        prenoms.forEach(System.out::println);
     }
 
     public static void methodeReferencee() {
